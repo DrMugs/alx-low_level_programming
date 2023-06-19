9e316aa7ea0778928717a5b3d39e622b504ee583
DrMugs <dmugwangi@gmail.com>
2023-06-20 01:34:57 +0200

    more alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int i;
+
+for (i = 0; i < 52; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
