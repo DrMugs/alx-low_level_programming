bec176b3c0f3502db4a60d1f95a52230d8537826
DrMugs <dmugwangi@gmail.com>
2023-06-21 00:18:57 +0200

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet()
+
+int main(void)
+{
+  char putC[] = "_putchar";
+
+  int i;
+
+  for (i = 0; i < 8; i++)
+  {
+    putchar(putC[i]);
+  }
+  putchar('\n');
+  return (0);
+}
