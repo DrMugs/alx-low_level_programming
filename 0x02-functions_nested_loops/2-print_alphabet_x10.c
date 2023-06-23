e0b2b93ce3bb8503dca654091d346ed61fa42b32
DrMugs <dmugwangi@gmail.com>
2023-06-23 23:22:51 +0200

    changes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+  char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+  int i;
+  int j;
+  for (j = 0; j < 10; i++)
+  {
+  for (i = 0; i < 26; i++)
+  {
+    _putchar(alphabet[i]);
+  }
+}
+
+  _putchar('\n');
+}
