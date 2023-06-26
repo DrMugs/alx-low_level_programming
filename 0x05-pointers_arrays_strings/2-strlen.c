54d7db057c941852515ce258e9548444a79294f6
DrMugs <dmugwangi@gmail.com>
2023-06-26 16:10:25 -0700

    legnth of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _strlen - calculate length of string
+ * @s : string whose lenth is measured
+ *
+ * Return: size of string as integer 0
+ */
+int _strlen(char *s)
+{
+	return (sizeof(*s) / sizeof(*s[0]));
+}
