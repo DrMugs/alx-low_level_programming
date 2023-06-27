013d712056887df72f27d89c560433e638cc3f3a
DrMugs <dmugwangi@gmail.com>
2023-06-26 18:48:40 -0700

    make string reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s : string to be reversed
+ */
+void rev_string(char *s)
+{
+	int k, i, j, counter;
+
+	char str[];
+
+	k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+		str[i] = s[i]
+	}
+
+	for (j = counter - 1; j >= 0; j--)
+	{
+		s[k] = str[j];
+		k++;
+	}
+
+}
