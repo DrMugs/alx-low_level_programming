19e0f41f2b08d01d362d5bea8ab102342b437230
DrMugs <dmugwangi@gmail.com>
2023-06-30 02:28:26 +0200

    2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,6 @@
 #define main_h
 
 int _putchar(char c);
-<<<<<<< HEAD
-=======
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,6 +12,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
->>>>>>> c891450d0f3073629ad30dc65647b8cb1e5803e6
 
 #endif
