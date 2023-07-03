89bc5d42466a64672485a2bb2bf03ebc22e3e561
DrMugs <dmugwangi@gmail.com>
2023-07-03 14:32:13 -0700

    allocate memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * _memset - allocates memory to given adress
+ * @s : adress pointer
+ * @b : characer to occupy block
+ * @n : number of bytes to be allocated
+ * Return:  pointer with memory set
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	s = (char *)malloc(n * sizeof(b));
+
+	return (s);
+}
