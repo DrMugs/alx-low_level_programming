1928cefb28238b302f167193968e380d8bbdcb3f
DrMugs <dmugwangi@gmail.com>
2023-07-05 15:29:19 -0700

    power recurursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * _pow_recursion - returns power raised tothe power of y
+ * @x : bse number
+ * @y : the exponent
+ * Return: the total
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
