e83ec6b14f0168ccfb7516ea15e45502fc1d54b2
DrMugs <dmugwangi@gmail.com>
2023-07-05 14:28:27 -0700

    reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _prinnt_rev_recursion - print stringinreverse
+ * @s : sting in question
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
