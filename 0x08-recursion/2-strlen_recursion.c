915d8637d7ec2543d96d28961036a476a647083d
DrMugs <dmugwangi@gmail.com>
2023-07-05 14:56:42 -0700

    count the strings

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * _strlen_recursion - return string lenght
+ * @s : string
+ * Return: legnth of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
+
+}
