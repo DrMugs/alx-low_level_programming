96cbec8d4cadcb1ffffc486c4fad7a0b9b20872e
DrMugs <37611611+DrMugs@users.noreply.github.com>
2023-07-06 01:48:08 +0200

    Update 5-sqrt_recursion.c
    switch

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns natural square-root number
- * @n : number to be rooted
- * Return: square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	else if (n == 0 || n == 1)
-		return (n);
-	else
-		return (sqroot(n, 2));
-}
-
-/**
  * sqroot - checks if sqaure root is avalable and returns if available
  * @x : the number
  * @y : control
@@ -31,4 +16,18 @@ int sqroot(int x, int y)
 			return (-1);
 	}
 	return (0 + sqroot(x, y + 1));
+}
+/**
+ * _sqrt_recursion - returns natural square-root number
+ * @n : number to be rooted
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (sqroot(n, 2));
 }
