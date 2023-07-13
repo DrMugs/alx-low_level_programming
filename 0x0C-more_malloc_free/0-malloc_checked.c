96bd832e6d953b0bd13198071361560a085ac8d4
DrMugs <dmugwangi@gmail.com>
2023-07-13 15:31:17 +0200

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b : the amount memory to be allocated
+ * Return: pointer to the adress
+ */
+void *malloc_checked(unsigned int b)
+{
+
