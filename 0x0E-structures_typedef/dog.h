62f5c96ed275c8ba22aa916517582876f21cba3b
DrMugs <dmugwangi@gmail.com>
2023-07-17 14:57:58 -0700

    new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: stores dog name, owner and age
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
