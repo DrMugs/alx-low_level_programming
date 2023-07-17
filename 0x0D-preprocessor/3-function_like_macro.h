d6d93f26ac8cdf8d2bbbf7022e606df749db739e
DrMugs <dmugwangi@gmail.com>
2023-07-17 11:53:29 -0700

    corect def

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCT_LIKE_MACRO_H
-#define FUNCT_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((X) < (0) ? (-X) : (X))
+#define ABS(X) ((X) < (0) ? -(X) : (X))
 
 #endif
