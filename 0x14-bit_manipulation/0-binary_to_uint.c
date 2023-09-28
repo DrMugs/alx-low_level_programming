ebfee9f47d9bc203f050062d238a2ba5762d1966
DrMugs <dmugwangi@gmail.com>
2023-09-28 22:19:35 +0200

    first one

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - makes binary number to numerial
+ * @b: the binary number
+ * Return: the numerial value or 0 otherwise
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int variable = 0;
+
+	if (!b)
+		return (0);
+
+	
+	while (*b)
+	{
+		if (*b == '0')
+		{
+			variable <<= 1;
+		}
+		else if (*b == '1')
+		{
+			variable = (variable << 1) | 1;
+		}
+		else
+			return (0);
+		b++;
+	}
+
+	return (variable);
+}
